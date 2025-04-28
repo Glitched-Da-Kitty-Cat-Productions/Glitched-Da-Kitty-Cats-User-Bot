d3dbc822d38fcbeda1b5376f97426117f826e549
Glitched Da Kitty Cat School Account <178820833+GlitchedDaKittyCatSchoolaccount@users.noreply.github.com>
2025-04-28 10:03:58 -0500

    e

--- config.js
+++ config.js
@@ -1,13 +1,12 @@
 module.exports = {
-    token: "MTI3NjA1OTkwNDU0ODkyOTU0OQ.GtYxm0.VqtUOrsY4V9jbGH6cSrROvF7YnP4D4wlwhjKTw", 
-    prefix: ".", 
-    allowedUserIDs: ["1276059904548929549"],
-    OPENROUTER_API_KEY: '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', // Go to https://openrouter.ai/ to get your API key [https://openrouter.ai/deepseek/deepseek-v3-base:free]
-    GROQ_API_KEY: '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', // Go to https://groq.com/ to get your API key
-    autoGuildTagSwitcher: false, // Enable or disable auto on guild tag switching
+    token: "Your Token", // Use this information to get your token https://gist.github.com/oyepriyansh/1be45b722181ec634664410cde244708
+    prefix: ".", // Change to whatever you want  
+    allowedUserIDs: ["Your User Id"], // to get your Discord user ID, you'll need to enable Developer Mode in your settings and then right-click on your profile picture in any channel or DM and select "Copy User ID". This will copy the ID to your clipboard, allowing you to paste it elsewhere. 
+    OPENROUTER_API_KEY: 'open_api_key', // Go to https://openrouter.ai/ to get your API key [https://openrouter.ai/deepseek/deepseek-v3-base:free]
+    GROQ_API_KEY: 'Groq Key', // Go to https://groq.com/ to get your API key
     interval: 10000, // Interval in milliseconds for auto guild tag switching
     guilds: {
-        "Dev": "547906569489350657",
-        "Meow": "427067963137589258"
+        "Guild 1": "Server ID",
+        "Guild 2": "Server Id"
     },
   };
